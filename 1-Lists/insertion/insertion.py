7804c9b8c66f0dfae051901302478491961fcb84
l33th <l33th@protonmail.ch>
2021-12-24 06:41:47 +0200

    insertion

---
+++ 1-Lists/insertion/insertion.py
@@ -0,0 +1,7 @@
+crypto_list = ["binance", "bitcoin", "ethereum", "monero"]
+
+crypto_list.append("monero-classic")
+
+crypto_list.insert(0, "bat")
+crypto_list.insert(3, "ethereum-classic")
+print(crypto_list)
