e5f469d6108f9488219f1f344dba0f64cb7b9c54
l33th <l33th@protonmail.ch>
2021-12-29 06:24:06 +0200

    built in tuple function negative

--- 2-Tuples/built-in-tuple-func/built-in-tuple-func.py
+++ 2-Tuples/built-in-tuple-func/built-in-tuple-func.py
@@ -1,5 +1,14 @@
 fn_tuple = tuple(["Sky", 42, "New York"])
 print(fn_tuple)
 
+# prints out '42'
 item = fn_tuple[1]
 print(item)
+
+# prints out 'New York'
+item2 = fn_tuple[-1]
+print(item2)
+
+# prints out '42'
+item3 = fn_tuple[-2]
+print(item3)
